edd25d96a1f50947f5766c11936b695dbbd6a379
pranaySpark18 <evincedevapi@gmail.com>
2023-01-22 17:28:52 +0530

    feature/added the components for the all users and active users

--- src/pages/ChatRoom/ChatRoom.jsx
+++ src/pages/ChatRoom/ChatRoom.jsx
@@ -1,39 +1,15 @@
 import React from 'react'
 import { Grid, Typography, TextField } from '@mui/material'
-import InputAdornment from '@mui/material/InputAdornment'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle'
-import SearchIcon from '@mui/icons-material/Search'
-import CBSearch from '../../components/Search/CBSearch'
 import './chatroom.css'
+import AllUser from './AllUser'
+import ActiveUser from './ActiveUser'
 function ChatRoom() {
   return (
     <>
       <Grid container spacing={2}>
         <Grid item xs={6} md={3} className="chatroom user-chat-list">
-          <div className="user-chat-search-bar">
-            <CBSearch
-              className="user-chat-search"
-              icon={<SearchIcon/>}
-            />         
-          </div>
-          <div className="user-chat-list-card">
-            <div>
-            <AccountCircleIcon
-              style={{ fontSize: '40px'}}
-            />
-            <Typography variant="h6" style={{ marginLeft: '12px' }}>
-              Pranay pratap singh
-            </Typography>
-            </div>
-            <div>
-            <AccountCircleIcon
-              style={{ fontSize: '40px'}}
-            />
-            <Typography variant="h6" style={{ marginLeft: '12px' }}>
-              Pranay pratap singh
-            </Typography>
-            </div>
-          </div>
+          <AllUser />
         </Grid>
         <Grid
           item
@@ -44,29 +20,8 @@ function ChatRoom() {
         >
           <div></div>
         </Grid>
-        <Grid
-          item
-          xs={6}
-          md={2}
-          className="chatroom"
-        >          
-          <div className='user-chat-active-head'>
-            <Typography variant='h6'>Active Users</Typography>
-          </div>
-          <div className="user-chat-list-card">
-            <div className="user-chat-list-card-container">
-              <AccountCircleIcon style={{ marginLeft: '5px' }} />
-              <Typography variant="body1" style={{ marginLeft: '12px' }}>
-                Pranay pratap singh
-              </Typography>
-            </div>
-            <div className="user-chat-list-card-container">
-              <AccountCircleIcon style={{ marginLeft: '5px' }} />
-              <Typography variant="body1" style={{ marginLeft: '12px' }}>
-                Pranay pratap singh
-              </Typography>
-            </div>
-          </div>
+        <Grid item xs={6} md={2} className="chatroom">
+          <ActiveUser />
         </Grid>
       </Grid>
     </>
