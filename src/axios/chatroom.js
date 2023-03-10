7a56c3b7abb9ae54aabb4a15cd4cd6d99223c286
pranaySpark18 <evincedevapi@gmail.com>
2023-03-10 18:09:38 +0530

    feature/fetch-message integrated

--- src/axios/chatroom.js
+++ src/axios/chatroom.js
@@ -1,8 +1,15 @@
 import axios from "./axios";
-import { getToken } from "../utils/localStorage";
+import { getToken , getUserId } from "../utils/localStorage";
 
 export const allUsers = () => {
   const token = getToken();
   const response = axios.get("/user/chatroom", { headers: { Authorization: `Bearer ${token}` } });
+  return response;
+};
+
+export const fetchChat = (receiver) => {
+  const token = getToken();
+  const loggedInUser = getUserId();
+  const response = axios.get(`/chat/${loggedInUser}/${receiver}`, { headers: { Authorization: `Bearer ${token}` } });
   return response;
 };
